ccdbcac6ce08433f6a534af61ea521692ff17ba2
Ariel Huang <ariel92911@gmail.com>
2019-10-17 23:03:31 +0800

    add home and user routers

--- routes/user.js
+++ routes/user.js
@@ -5,6 +5,10 @@ const passport = require('passport')
 //const bcrypt = require('bcryptjs')
 //const User = require('../models/user')
 
+// 載入 user model
+const db = require('../models')
+const User = db.User
+
 // 登入頁面
 router.get('/login', (req, res) => {
   res.render('login')
@@ -12,10 +16,11 @@ router.get('/login', (req, res) => {
 
 // 登入檢查
 router.post('/login', (req, res, next) => {
-  passport.authenticate('local', {                        // 使用 passport 認證
-    successRedirect: '/',                                         // 登入成功會回到根目錄
-    failureRedirect: '/users/login'                        // 失敗會留在登入頁面
-  })(req, res, next)
+  res.send('登入檢查')
+  // passport.authenticate('local', {                        // 使用 passport 認證
+  //   successRedirect: '/',                                         // 登入成功會回到根目錄
+  //   failureRedirect: '/users/login'                        // 失敗會留在登入頁面
+  // })(req, res, next)
 })
 
 // 註冊頁面
@@ -27,67 +32,69 @@ router.get('/register', (req, res) => {
 router.post('/register', (req, res) => {
   const { name, email, password, password2 } = req.body
 
-  // 加入錯誤訊息提示
-  let errors = []
-  if (!name || !email || !password || !password2) {
-    errors.push({ message: '所有欄位都是必填' })
-  }
+  // // 加入錯誤訊息提示
+  // let errors = []
+  // if (!name || !email || !password || !password2) {
+  //   errors.push({ message: '所有欄位都是必填' })
+  // }
 
-  if (password !== password2) {
-    errors.push({ message: '密碼輸入錯誤' })
-  }
+  // if (password !== password2) {
+  //   errors.push({ message: '密碼輸入錯誤' })
+  // }
 
-  if (errors.length > 0) {
-    res.render('register', {
-      errors,
-      name,
-      email,
-      password,
-      password2
-    })
-  } else {
-    User.findOne({ email: email }).then(user => {
-      if (user) {
-        // 加入訊息提示
-        errors.push({ message: '這個 Email 已經註冊過了' })
-        res.render('register', {
-          name,
-          email,
-          password,
-          password2
+  // if (errors.length > 0) {
+  //   res.render('register', {
+  //     errors,
+  //     name,
+  //     email,
+  //     password,
+  //     password2
+  //   })
+  // } else {
+  User.findOne({ where: { email: email } }).then(user => {
+    if (user) {
+      // 加入訊息提示
+      console.log('User already exists')
+      //errors.push({ message: '這個 Email 已經註冊過了' })
+      res.render('register', {
+        name,
+        email,
+        password,
+        password2
+      })
+    } else {
+      const newUser = new User({
+        name,
+        email,
+        password
+      })
+
+      //       // 先用 genSalt 產生「鹽」，第一個參數是複雜度係數，預設值是 10
+      //       bcrypt.genSalt(10, (err, salt) =>
+      //         // 再用 hash 把鹽跟使用者的密碼配再一起，然後產生雜湊處理後的 hash
+      //         bcrypt.hash(newUser.password, salt, (err, hash) => {
+      //           if (err) throw err
+      //           newUser.password = hash
+
+      newUser
+        .save()
+        .then(user => {
+          res.redirect('/')
         })
-      } else {
-        const newUser = new User({
-          name,
-          email,
-          password
-        })
-
-        // 先用 genSalt 產生「鹽」，第一個參數是複雜度係數，預設值是 10
-        bcrypt.genSalt(10, (err, salt) =>
-          // 再用 hash 把鹽跟使用者的密碼配再一起，然後產生雜湊處理後的 hash
-          bcrypt.hash(newUser.password, salt, (err, hash) => {
-            if (err) throw err
-            newUser.password = hash
-
-            newUser
-              .save()
-              .then(user => {
-                res.redirect('/')
-              })
-              .catch(err => console.log(err))
-          })
-        )
-      }
-    })
-  }
+        .catch(err => console.log(err))
+      //         })
+      //       )
+    }
+  })
+  // }
 })
 
 // 登出
 router.get('/logout', (req, res) => {
-  req.logout()
-  req.flash('success_msg', '你已經成功登出')
-  res.redirect('/users/login')
+  res.send('logout')
+  // req.logout()
+  // req.flash('success_msg', '你已經成功登出')
+  // res.redirect('/users/login')
 })
 
 module.exports = router
