72015e09249154aad4d4fd3af908d0f94a2741dc
Ariel Huang <ariel92911@gmail.com>
2019-10-17 20:11:56 +0800

    add routes and views for register and login pages

--- app.js
+++ app.js
@@ -19,6 +19,32 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.get('/', (req, res) => {
   res.send('hello world')
 })
+// 認證系統的路由
+// 登入頁面
+app.get('/users/login', (req, res) => {
+  res.render('login')
+})
+// 登入檢查
+app.post('/users/login', (req, res) => {
+  res.send('login')
+})
+// 註冊頁面
+app.get('/users/register', (req, res) => {
+  res.render('register')
+})
+// 註冊檢查
+app.post('/users/register', (req, res) => {
+  res.send('register')
+})
+// 登出
+app.get('/users/logout', (req, res) => {
+  res.send('logout')
+})
+
+// 載入路由器
+//app.use('/', require('./routes/home'))
+//app.use('/users', require('./routes/user'))
+
 
 // 設定 express port 3000
 app.listen(port, () => {
