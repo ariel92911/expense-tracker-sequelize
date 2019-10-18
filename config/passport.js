b15c3b0f4428e7cfc7cdb155090f315234b2dbe2
Ariel Huang <ariel92911@gmail.com>
2019-10-18 21:53:41 +0800

    Add bcrypt to login route

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,6 @@
 // config/passport.js
 const LocalStrategy = require('passport-local').Strategy
+const bcrypt = require('bcryptjs')
 
 // 載入 User model
 const db = require('../models')
@@ -13,11 +14,14 @@ module.exports = passport => {
           if (!user) {
             return done(null, false, { message: 'That email is not registered' })
           }
-          if (user.password != password) {
-            console.log('user password not correct.')
-            return done(null, false, { message: 'Email or Password incorrect' })
-          }
-          return done(null, user)
+          bcrypt.compare(password, user.password, (err, isMatch) => {
+            if (err) throw err
+            if (isMatch) {
+              return done(null, user)
+            } else {
+              return done(null, false, { message: 'Email and Password incorrect' })
+            }
+          })
         })
     })
   )
